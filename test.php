6b69b2813d67713baf937e5f38b1f695337e5c1a
loivcavil <lovcavil@hotmail.com>
2012-06-19 19:44:53 +0800

    teat01

--- test.php
+++ test.php
@@ -5,6 +5,25 @@ if (!$con)
   die('Could not connect: ' . mysql_error());
   }
 
-// some code
-echo "aaaa"
+// Create database
+if (mysql_query("CREATE DATABASE my_db",$con))
+  {
+  echo "Database created";
+  }
+else
+  {
+  echo "Error creating database: " . mysql_error();
+  }
+
+// Create table in my_db database
+mysql_select_db("my_db", $con);
+$sql = "CREATE TABLE Persons 
+(
+FirstName varchar(15),
+LastName varchar(15),
+Age int
+)";
+mysql_query($sql,$con);
+
+mysql_close($con);
 ?>
