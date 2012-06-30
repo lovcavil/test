590a65267017a96482c51c6ecd87b1aa657035b1
loivcavil <lovcavil@hotmail.com>
2012-06-30 15:29:58 +0800

    upload char
    need fix to fit utf8

---
+++ recieve.php
@@ -0,0 +1,19 @@
+﻿
+<?php
+header("Content-Type:text/html;charset=UTF-8");
+$con = mysql_connect("localhost","root","qwerty");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("my_db", $con);
+mysql_query("SET NAMES UTF-8");
+$sql="INSERT INTO linetable (lid,text) VALUES (1,'$_POST[line]')";
+mysql_query($sql);
+
+mysql_query("INSERT INTO `my_db`.`linetable` (`lid`, `text`) VALUES (NULL, '啊啊啊')");
+
+
+mysql_close($con);
+?>
